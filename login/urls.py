5b10c7df6a5e12c453138a1dcb338072e46444db
renatojobal <renatojobal@gmail.com>
2019-11-13 19:49:29 -0500

    Agregando enrutador

--- login/urls.py
+++ login/urls.py
@@ -6,26 +6,6 @@ from rest_framework_jwt.views import obtain_jwt_token
 from django.conf.urls.static import static
 from django.conf import settings
 
-#router = routers.DefaultRouter()
-#router.register('login', views.login, name="login")
-#router.register('usuario', views.usuario)
-'''
-router.register('category', views.Category)
-router.register('itemcategory', views.ItemCategory)
-router.register('persons', views.Persons)
-router.register('personsDepartments', views.Persons_departaments)
-router.register('personsRole', views.Persons_role)
-router.register('personsMedia', views.Persons_media)
-router.register('personsContact', views.Persons_Contacts)
-router.register('subjectMatter', views.Subject_matter)
-router.register('preRequirements', views.Pre_requirements)
-router.register('infoSite', views.Info_site)
-router.register('content', views.Content)
-router.register('contentMedia', views.Content_media)
-router.register('contentInfo', views.Content_info)
-router.register('menu', views.Menu)
-'''
-#router.register('titulacion',views.ItemCategoryRolList)
 
 urlpatterns = [
     path('', views.vista1, name="vista1"),
