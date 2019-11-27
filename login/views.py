b2b00441a6d0af5e2b7dc8447a202618b442cba1
renatojobal <renatojobal@gmail.com>
2019-11-27 10:23:32 -0500

    Agregando importaciones de permisos

--- login/views.py
+++ login/views.py
@@ -2,7 +2,7 @@
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 
