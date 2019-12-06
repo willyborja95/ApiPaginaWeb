19173eef0aeda62fdab8a54c02bb9f5acb3467a2
renatojobal <renatojobal@gmail.com>
2019-12-06 00:16:54 -0500

    Creando clase para customizar los permisos

--- login/authentication.py
+++ login/authentication.py
@@ -1,5 +1,17 @@
 # Simple_jwt imports
 from rest_framework_simplejwt import authentication
 
+# Rest framework imports
+from rest_framework import permissions
+
+class CustomerAccessPermission(permissions.BasePermission):
+    message = 'Adding customers not allowed.'
+
+    def has_permission(self, request, view):
+         ...
+
+
+
+
 class JWTTokenUserAuthentication(authentication.JWTAuthentication):
     pass
