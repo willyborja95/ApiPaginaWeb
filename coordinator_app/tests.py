ee31adf7a2eb41b2ed244c4f23a223294a79677a
renatojobal <renatojobal@gmail.com>
2019-12-17 22:57:05 -0500

    Creando estrucutra para crear los servicios

--- coordinator_app/tests.py
+++ coordinator_app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Django importaciones
+# ...
+
+# Rest_framework importaciones
+# ...
+
+# Local importaciones
+# ...
+
+# Otras importaciones
+# '''
